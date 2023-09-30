772d57a0d83da619a5d2f1830ca7bc4b7a4457f4
ShinyMyst <122401790+ShinyMyst@users.noreply.github.com>
2023-09-29 22:50:10 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+function main() {
+  console.log(INDEX_MAP["What time do you expect to return the DBC to Kiefaber 461?"])
+  for (var row of DATA.slice(1)) {
+    createEntry(row)
+    modifyEntry(row)
+  }
+}
+
+
+function createEntry(row) {
+}
+
+function modifyEntry(row) {
+
+}
