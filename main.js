a415dadee7bd2d6c1ebfd0993f5574a9b06f01bd
ShinyMyst <122401790+ShinyMyst@users.noreply.github.com>
2023-10-04 22:33:21 -0400

    Update main.js
    Modify and create events.

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 function main() {
-  console.log(INDEX_MAP["What time do you expect to return the DBC to Kiefaber 461?"])
   for (var row of DATA.slice(1)) {
     createEntry(row)
     modifyEntry(row)
@@ -8,8 +7,49 @@ function main() {
 
 
 function createEntry(row) {
+  // If calendar link is empty, create a new calendar entry
+  // Write to last modified and calendar link
+  if (row[EVENT_LINK] == "") {
+    console.log("Making an entry")
+    // Prep Date
+    const startDate = new Date(row[START_DATE]);
+    const endDate = new Date(row[END_DATE]);
+    const startTime = row[START_TIME];
+    const endTime = row[END_TIME];
+
+    console.log(startDate)
+    console.log(endDate)
+    console.log(startTime)
+    console.log(endTime)
+
+
+    startDate.setHours(startTime.getHours(), startTime.getMinutes());
+    endDate.setHours(endTime.getHours(), endTime.getMinutes());
+
+    console.log(startDate)
+    console.log(endDate)
+
+    const event = CALENDAR.createEvent({
+      title: row[EVENT_NAME],
+      start: startDate,
+      end: endDate,
+      });
+  }
 }
 
 function modifyEntry(row) {
+  // If last modified does not match modified
+  // Use the calendar link to retreive and edit event
+  if (row[TIMESTAMP] !== row[MODIFIED]) {
+    console.log("Modify")
+  } 
+}
 
+//
+function createCalendarEntry(row){
+
+}
+
+function modifyCalendarEntry(row){
+  
 }
