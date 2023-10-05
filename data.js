d8e505320d2c15fc92ae8f50221be1e701a8c2e9
ShinyMyst <122401790+ShinyMyst@users.noreply.github.com>
2023-10-04 22:33:43 -0400

    Update data.js
    Retrieve data via index.

--- data.js
+++ data.js
@@ -5,49 +5,58 @@
 
 const spreadsheetId = "1WA3hyaWy-PqwjDchFnd7wRAKnVbRhwHrBSLey8fItP0"
 const pageName = "Form Responses 1"
+const calendarId = "c_a9ecb61fd391901406217a1abf70f4d439d1feead01b9f6b15cf6a08dd401d3f@group.calendar.google.com"
 
 // Copy and paste the question that provides the associated information
-const questionRetreiveDate = "What date will you be picking up the DBC from Kiefaber 461?"
-const questionRetrieveTime = "What time will you be picking up the DBC from Kiefaber 461?"
-const questionReturnDate = "What date do you expect to return the DBC to Kiefaber 461?"
-const questionReturnTime = "What time do you expect to return the DBC to Kiefaber 461?"
-const questionEventName = "Name of event"
-const questionPurchaser = "Your Name"
-const questionArea = "Name of Area Council offering this submission"
-const questionOriginalTimeStamp = "Timestamp"
-const questionModifiedTimeStamp = "Last Modified"
-const questionCalendarLink = "Calendar Link"
+const startDate = "What date will you be picking up the DBC from Kiefaber 461?"
+const startTime = "What time will you be picking up the DBC from Kiefaber 461?"
+const endDate = "What date do you expect to return the DBC to Kiefaber 461?"
+const endTime = "What time do you expect to return the DBC to Kiefaber 461?"
+const eventName = "Name of event"
+const purchaser = "Your Name"
+const area = "Name of Area Council offering this submission"
+const timestamp = "Timestamp"
+const modified = "Last Modified"
+const eventLink = "Calendar Link"
+
 
 // ###################
-// Automated Data Set-Up
+// Get Data & Calendar
 // ###################
-// This sets up the data for the script to use.  
-// Do NOT touch information here unless you know what you're doing.
-
 const activeSheet = SpreadsheetApp.openById(spreadsheetId);
 const activePage = activeSheet.getSheetByName(pageName);
 
 const DATA = activePage.getDataRange().getValues();
-const INDEX_MAP = createHeaderToIndexMap()
+const CALENDAR = CalendarApp.getCalendarById(calendarId)
 
 // ###################
-// Data Points
+// Data Indicies
 // ###################
-// Using variable rather than the string directly to allow the portion editable
-// by RHA member to be seperate from the functional code.
-const modifiedTimeStamp = INDEX_MAP[questionModifiedTimeStamp]
-const calendarLink = INDEX_MAP[questionCalendarLink]
+// Store the index of specific data points.
+// Some redundancy using var instead of the text directly
+// This is done to seperate the value that general users can tweak.
+const index_map = createHeaderToIndexMap()
+
+const START_DATE = index_map[startDate];
+const START_TIME = index_map[startTime];
+const END_DATE = index_map[endDate];
+const END_TIME = index_map[endTime];
+const EVENT_NAME = index_map[eventName];
+const PURCHASER = index_map[purchaser];
+const AREA = index_map[area];
+const TIMESTAMP = index_map[timestamp];
+const MODIFIED = index_map[modified];
+const EVENT_LINK = index_map[eventLink];
 
 // ###################
 // Functions
 // ###################
-// ChatGPT Function
 function createHeaderToIndexMap() {
   var headers = activePage.getRange(1, 1, 1, activePage.getLastColumn()).getValues()[0];
   var headerToIndex = {};
 
   for (var i = 0; i < headers.length; i++) {
-    headerToIndex[headers[i]] = i + 1; // Add 1 to the index to make it 1-based.
+    headerToIndex[headers[i]] = i
   }
 
   return headerToIndex;
