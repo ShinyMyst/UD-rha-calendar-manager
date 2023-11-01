cb2c97f4d0bfc6fd198ff47d2128160ca98ee3ad
ShinyMyst <122401790+ShinyMyst@users.noreply.github.com>
2023-11-01 11:00:24 -0400

    Can access ID directly instead of by URL now.
    Storing URL did not function correctly so store ID isntead.

--- spreadsheet.js
+++ spreadsheet.js
@@ -2,13 +2,11 @@ function writeTimestamps(rowIndex){
   // Updates the Timestamp and Last Modified columns to current date.
   const currentDate = new Date();
   const formattedDate = Utilities.formatDate(currentDate, 'GMT', 'MM/dd/yyyy HH:mm:ss');
-  console.log(getColumn(header.formTimestamp))
   PAGE.getRange(rowIndex, getColumn(header.formTimestamp)).setValue(formattedDate);
   PAGE.getRange(rowIndex, getColumn(header.scriptTimestamp)).setValue(formattedDate);
 };
 
 function writeEventLink(rowIndex, event){
   // Updates the event URL column to given an event entity
-  var eventURL = `https://calendar.google.com/calendar/event?eid=${event.getId()}`;
-  PAGE.getRange(rowIndex, getColumn(header.eventLink)).setValue(eventURL);
+  PAGE.getRange(rowIndex, getColumn(header.eventLink)).setValue(event.getId());
 };
